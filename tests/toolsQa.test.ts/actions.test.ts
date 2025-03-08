7f78d5782df0f0ddf56b3bef4ba00b66ad2f0a4f
sheikrasheedAhmed <110345126+sheikrasheedAhmed@users.noreply.github.com>
2025-03-08 14:23:17 +0530

    added a new line

--- tests/toolsQa.test.ts/actions.test.ts
+++ tests/toolsQa.test.ts/actions.test.ts
@@ -4,7 +4,7 @@ test('actions', async ({ page })=> {
     await page.goto('https://demoqa.com/'), {timeout:50000};
     await page.getByRole('heading',{name:'Elements'});
     await page.fill('getByText[name="Text Box"]','playwrightuser');
-
+//added a line
 });
   
 
